c48134adc4d8878ad1ebbaf595bc1f95446d738c
jkraszewski <jkraszewski15@yahoo.com>
2015-12-05 16:16:58 -0500

    message system mk2

--- message.js
+++ message.js
@@ -1,4 +1,4 @@
 funtion AddText(){
   var str = document.getElementById("message").value.trim();
-  document.getElementById("display").innerHTML = str;
+  document.getElementById("displayMessage").innerHTML = str;
 }
